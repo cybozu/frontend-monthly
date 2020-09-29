60d860529a8d84ae5059e003b767a236e605b862
Toru Kobayashi <koba0004@gmail.com>
2020-09-30 00:20:50 +0900

    feat: display thumnails for guests

---
+++ site/src/components/Member.js
@@ -0,0 +1,37 @@
+import { withPrefix } from "gatsby"
+import React from "react"
+import styled from "@emotion/styled"
+
+const Component = ({ member: {　name, link }, className }) => {
+  return (
+    <div className={`${className}`}>
+      <img
+        src={withPrefix(`/img/${name}.jpg`)}
+        className="thumbnail"
+        alt={name}
+      />
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        {name}
+      </a>
+    </div>
+  )
+}
+
+const StyledComponent = styled(Component)`
+  box-sizing: border-box;
+  display: flex;
+  flex-basis: 20%;
+  flex-direction: column;
+  margin: 15px;
+  text-align: center;
+  min-width: 105px;
+
+  .thumbnail {
+    max-width: 100px;
+    height: auto;
+    border-radius: 50%;
+    margin: 0 auto 5px;
+  }
+`
+
+export const Member = StyledComponent
--- site/src/components/TeamMembers.js
+++ site/src/components/TeamMembers.js
@@ -1,37 +1,21 @@
-import { withPrefix } from "gatsby"
 import React from "react"
 import styled from "@emotion/styled"
+
+import { Member } from "./Member"
 
 const Component = ({ members, guest, className }) => {
   return (
     <>
       <h2>チーム紹介</h2>
       <div className={`${className} members`}>
-        {members.map(({ name, link }) => (
-          <div key={name} className="member">
-            <img
-              src={withPrefix(`/img/${name}.jpg`)}
-              className="thumbnail"
-              alt={name}
-            />
-            <a href={link} target="_blank" rel="noopener noreferrer">
-              {name}
-            </a>
-          </div>
-        ))}
+        {members.map((member) => <Member key={member.name} member={member} />)}
       </div>
       {guest && guest.length && (
         <>
           <h2>ゲスト</h2>
-          <ul>
-            {guest.map(({ name, link, comment }) => (
-              <li key={name}>
-                <a href={link} target="_blank" rel="noopener noreferrer">
-                  {name}
-                </a>
-              </li>
-            ))}
-          </ul>
+          <div className={`${className} members`}>
+            {guest.map((guest) => <Member key={guest.name} member={guest} />)}
+          </div>
         </>
       )}
     </>
@@ -42,25 +26,6 @@ const StyledComponent = styled(Component)`
   display: flex;
   flex-wrap: wrap;
   box-sizing: border-box;
-  justify-content: center;
-
-  .member {
-    flex-grow: 1;
-    flex-basis: 20%;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-    margin: 15px;
-    text-align: center;
-    min-width: 105px;
-  }
-  .thumbnail {
-    max-width: 100px;
-    width: 100%;
-    height: auto;
-    border-radius: 50%;
-    margin: 0 auto 5px;
-  }
 `
 
 export const TeamMembers = StyledComponent
