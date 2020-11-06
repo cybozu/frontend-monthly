d75138bba73d3b8801582952611356b8f1ca5eaf
nakajmg <nakajima.jmg@gmail.com>
2020-11-06 14:54:41 +0900

    配信URLを設定できるように

---
+++ site/src/components/StreamView.js
@@ -0,0 +1,34 @@
+import React from "react"
+import styled from "@emotion/styled"
+
+const Component = ({ streamUrl, className }) => {
+  const isYouTube = /youtube\.com/.test(streamUrl)
+  const embedUrl = isYouTube ? streamUrl.replace("watch?v=", "embed/") : ""
+  return (
+    <p className={className}>
+      {embedUrl && (
+        <iframe
+          title={embedUrl}
+          width="560"
+          height="315"
+          src={embedUrl}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="embed"
+        />
+      )}
+
+      <a href={streamUrl} target="_blank" rel="noreferrer noopner">
+        {streamUrl}
+      </a>
+    </p>
+  )
+}
+
+const StyledComponent = styled(Component)`
+  .embed {
+    display: block;
+  }
+`
+export const StreamView = StyledComponent
