0b7cd7047859b7a82e5a0292491905b7a778abf1
Naohiro Nakajima <nakajima.jmg@gmail.com>
2021-07-05 12:23:59 +0900

    Revert "Revert "Revert "replace gatsby to next"""

--- lib/withPrefix.ts
+++
@@ -1,7 +0,0 @@
-import getConfig from "next/config";
-import path from "path"
-const { publicRuntimeConfig  } = getConfig();
-
-export const withPrefix = (href: string) => {
-  return path.join(publicRuntimeConfig.basePath, href)
-}
--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  basePath: process.env.BASE_PATH || "",
-  reactStrictMode: true,
-  assetPrefix: process.env.BASE_PATH || "",
-  publicRuntimeConfig: {
-    basePath: process.env.BASE_PATH || "",
-  },
-  trailingSlash: true,
-};
