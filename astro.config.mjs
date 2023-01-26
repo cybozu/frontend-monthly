426b6d8f6a14b475ce25e6a1ac75772e8b23cd4e
nus3 <hadayota33@gmail.com>
2023-01-26 17:25:33 +0900

    remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-import react from "@astrojs/react"
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
   output: "static",
   markdown: {
     syntaxHighlight: "prism",
