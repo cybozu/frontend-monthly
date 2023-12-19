6de84ef5856fb1726de48537b5688dd18af3c286
nakajmg <nakajima.jmg@gmail.com>
2023-12-19 20:20:15 +0900

    topicsをcontentに

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { z, defineCollection } from 'astro:content'
+import { z, defineCollection } from "astro:content"
 
 const postsCollection = defineCollection({
   schema: z.object({
@@ -8,17 +8,37 @@ const postsCollection = defineCollection({
     streamUrl: z.string().nullable().optional(),
     hashTag: z.string().nullable().optional(),
     no: z.number(),
-    members: z.object({
-      name: z.string(),
-      link: z.string().nullable().optional()
-    }).array(),
-    guest: z.object({
-      name: z.string(),
-      link: z.string().nullable().optional()
-    }).array().nullable().optional(),
+    members: z
+      .object({
+        name: z.string(),
+        link: z.string().nullable().optional(),
+      })
+      .array(),
+    guest: z
+      .object({
+        name: z.string(),
+        link: z.string().nullable().optional(),
+      })
+      .array()
+      .nullable()
+      .optional(),
   }),
 })
 
+const topicsCollection = defineCollection({
+  type: "data",
+  schema: z.array(
+    z.object({
+      title: z.string(),
+      date: z.string(),
+      overview: z.string(),
+      url: z.string().optional(),
+      isNotable: z.boolean().optional(),
+    })
+  ),
+})
+
 export const collections = {
-  'posts': postsCollection,
+  posts: postsCollection,
+  lookback: topicsCollection,
 }
